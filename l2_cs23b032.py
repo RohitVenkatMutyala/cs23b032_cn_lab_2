a0b6271c84d07a46be628f20cd5298e39733d981
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 <rohithvenkatmutyala@gmail.com>
2026-01-13 15:51:13 +0530

    analytical funcion implementation

--- l2_cs23b032.py
+++ l2_cs23b032.py
@@ -4,4 +4,11 @@ p_values = [i / 10 for i in range(1, 11)]
 print("Slotted ALOHA Simulation")
 print("Number of nodes:", n)
 print("p values:", p_values)
+def analytical_success(n, p):
+    return n * p * ((1 - p) ** (n - 1))
+analytical_results=[]
+for p in p_values:
+    analytical_results.append(analytical_success(n, p))
 
+print("Analytical success probabilities:")
+print(analytical_results)
